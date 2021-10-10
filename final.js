88e65a02015cb07344104d1f72c0b1fab4133ef0
nuviba <nuriaviba93@hotmail.com>
2021-10-10 20:55:16 +0200

    añadiendo error en el input

--- final.js
+++ final.js
@@ -39,6 +39,114 @@ function handleResponse(response) {
         });
 }
 //--------------------CÓDIGO PROPIO--------------------
+//muesrta todos los elementos de la opción que selecciona el usuario
+function allFromCategory (){
+    if (localStorage.getItem('dataZelda')!=null){
+        data = JSON.parse(localStorage.getItem('dataZelda'));
+        let catSelect=document.getElementById("selectCategory").value;
+            let catObject; //array con todos los elementos de una categoría
+            
+            if(catSelect=="creatures-food"){
+                catObject=data.data.creatures.food;
+            }
+            else if(catSelect=="creatures-non-food"){
+                catObject=data.data.creatures.non_food;
+            }
+            else{
+                catObject=data.data[catSelect];
+            }
+            document.getElementById("templateCard").innerHTML="";
+        //aquí recorremos todos los elementos de la categoría y los mostramos con la función "tempateCard"
+        for (let i=0;i<catObject.length;i++){
+            templateCard(i, catObject[i]);
+        }
+
+    }
+    else{
+    fetch('https://botw-compendium.herokuapp.com/api/v2').then(handleResponse)
+        .then(function cogerData(data){
+            let catSelect=document.getElementById("selectCategory").value;
+            let catObject;
+            
+            if(catSelect=="creatures-food"){
+                catObject=data.data.creatures.food;
+            }
+            else if(catSelect=="creatures-non-food"){
+                catObject=data.data.creatures.non_food;
+            }
+            else{
+                catObject=data.data[catSelect];
+            }
+            document.getElementById("templateCard").innerHTML="";
+        for (let i=0;i<catObject.length;i++){
+            templateCard(i, catObject[i]);
+        }
+    localStorage.setItem('dataZelda',JSON.stringify(data));
+}).catch(error => {
+
+    // This error object will have the error from the server
+    // As well as the two additions we made earlier of the status and statusText
+    
+    document.getElementById("templateCard").innerHTML=`<div class=errDiv><h1>${error}</h1><h2>Try it again later. Ganon is waiting for you!!</h2></div>`;
+})
+    }
+
+};
+//función que muestra un elemento de la búsqueda
+function templateCard(id, objeto){
+//creamos un div y le damos forma, introducimos los datos comunes a todas las categorías (nombre, imagen y icono like)
+document.getElementById("templateCard").innerHTML+="<div class=card id="+id+"></div>";
+document.getElementById("templateCard").style.margin="10px";
+document.getElementById("templateCard").style.marginBottom="10px";
+document.getElementById("templateCard").style.fontFamily="fantasy";
+document.getElementById("templateCard").style.color="#624e29";
+//en el nombre del item añadimos un icono para guardar ese elemento en favoritos, el icono llama una función "addFav"
+document.getElementById(id).innerHTML+="<div class=className><h1>"+objeto.name+"</h1><img onclick='addFav("+id+")' src='./img/h3.png' alt='heartLike'></div>";
+document.getElementById(id).innerHTML+=`<img src=${objeto.image} alt="zelda objet">`;
+//como alguna elementos la localización en la API es null, hacemos este condicional
+if(objeto.common_locations!=null){
+    document.getElementById(id).innerHTML+=("<p><b>Locations: </b> "+objeto.common_locations+"</p>").replace(",",", ");}
+//cada categoría tiene información específica, así que dependiendo de la categoría del objeto añadimos unas cosas u otras al div
+let catSelect= document.getElementById("selectCategory").value;
+switch(catSelect){
+    case `creatures-food`:
+        document.getElementById(id).innerHTML+="<p><b>Cooking effect: </b>"+objeto.cooking_effect+"</p>";
+        document.getElementById(id).style.background="#fbeddfb2";
+        document.getElementById(id).style.border="solid #c58f99";            
+        break;
+    case `creatures-non-food`:
+        document.getElementById(id).innerHTML+=("<p><b>Drop: </b>"+objeto.drops+"</p>").replace(",",", ");
+        document.getElementById(id).style.background="#fbeddfb2";
+        document.getElementById(id).style.border="solid #2b4173";
+        break;
+    case `equipment`:
+        document.getElementById(id).innerHTML+="<p><b>Attack: </b>"+objeto.attack+"</p>";
+        document.getElementById(id).innerHTML+="<p><b>Defense: </b>"+objeto.defense+"</p>";
+        document.getElementById(id).style.background="#fbeddfb2";
+        document.getElementById(id).style.border="solid #471f18";
+        break;
+    case `materials`:
+        document.getElementById(id).innerHTML+="<p><b>Cooking effect: </b>"+objeto.cooking_effect+"</p>";
+        document.getElementById(id).innerHTML+="<p><b>Hearts recovered: </b>"+objeto.hearts_recovered+"</p>";
+        document.getElementById(id).style.background="#fbeddfb2";
+        document.getElementById(id).style.border="solid #4c94b4";
+        break;
+    case `monsters`:
+        document.getElementById(id).innerHTML+=("<p><b>Drop: </b>"+objeto.drops+"</p>").replace(",",", ");
+        document.getElementById(id).style.background="#fbeddfb2";
+        document.getElementById(id).style.border="solid #5a4c84";
+        break;
+    case `treasure`:
+        document.getElementById(id).innerHTML+=("<p><b>Drop: </b>"+objeto.drops+"</p>").replace(",",", ");
+        document.getElementById(id).style.background="#fbeddfb2";
+        document.getElementById(id).style.border="solid #ba9b63";
+        break;
+    default: 
+}
+//finalemente añadimos la descripción general que comparten todos los elementos
+document.getElementById(id).innerHTML+="<p>"+objeto.description+"</p>";
+
+}
 //FUNCIÓN FILTRO PARA BUSCAR POR CATEGORÍA Y FILTRAR POR NOMBRE, LOCALIZACIÓN Y DESCRIPCIÓN
 function searchByCategory (tipo){ 
     //primero comprobamos si los datos están LS
@@ -121,144 +229,53 @@ function searchByCategory (tipo){
 //funciones de filtro
     function searchByName(lista){
         buscar=document.getElementById("searchNam").value;
+        let prueba=false;
         //busca en toda la array si coninciden los carácteres
         for (let i=0;i<lista.length;i++){
             //si coinciden, llama al a función que muestre el resultado
             if(lista[i].name.toLowerCase().includes(buscar.toLowerCase())){
-                templateCard(i,lista[i])}
+                templateCard(i,lista[i])
+                prueba=true;
+            }
+        }
+        if (prueba==false){
+            document.getElementById("templateCard").innerHTML=`<div class=errDiv><h2>No items with typed name, please try again</h2></div>`;
         }
         
     }
 
     function searchByLoc(lista){
         buscar=document.getElementById("searchLoc").value;
+        let prueba=false;
         
         for (let i=0;i<lista.length;i++){
             if(lista[i].common_locations!=null){
                 for(let j=0;j<lista[i].common_locations.length;j++){
                     if(lista[i].common_locations[j].toLowerCase().includes(buscar.toLowerCase())){
                         templateCard(i,lista[i])
+                        prueba=true;
                         break;}
                 }           
                 }
             } 
+            if (prueba==false){
+                document.getElementById("templateCard").innerHTML=`<div class=errDiv><h2>No items with typed location, please try again</h2></div>`;
+            }
     }
 
     function searchByDes(lista){
         buscar=document.getElementById("searchDes").value;
+        let prueba=false;
         for (let i=0;i<lista.length;i++){
             if(lista[i].description.toLowerCase().includes(buscar.toLowerCase())){
-                templateCard(i,lista[i])}
+                templateCard(i,lista[i])
+                prueba=true;}
+        }
+        if (prueba==false){
+            document.getElementById("templateCard").innerHTML=`<div class=errDiv><h2>No items with typed description, please try again</h2></div>`;
         }    
     }
-//muesrta todos los elementos de la opción que selecciona el usuario
-function allFromCategory (){
-        if (localStorage.getItem('dataZelda')!=null){
-            data = JSON.parse(localStorage.getItem('dataZelda'));
-            let catSelect=document.getElementById("selectCategory").value;
-                let catObject;
-                
-                if(catSelect=="creatures-food"){
-                    catObject=data.data.creatures.food;
-                }
-                else if(catSelect=="creatures-non-food"){
-                    catObject=data.data.creatures.non_food;
-                }
-                else{
-                    catObject=data.data[catSelect];
-                }
-                document.getElementById("templateCard").innerHTML="";
-            //aquí recorremos todos los elementos de la categoría y los mostramos con la función "tempateCard"
-            for (let i=0;i<catObject.length;i++){
-                templateCard(i, catObject[i]);
-            }
-    
-        }
-        else{
-        fetch('https://botw-compendium.herokuapp.com/api/v2').then(handleResponse)
-            .then(function cogerData(data){
-                let catSelect=document.getElementById("selectCategory").value;
-                let catObject;
-                
-                if(catSelect=="creatures-food"){
-                    catObject=data.data.creatures.food;
-                }
-                else if(catSelect=="creatures-non-food"){
-                    catObject=data.data.creatures.non_food;
-                }
-                else{
-                    catObject=data.data[catSelect];
-                }
-                document.getElementById("templateCard").innerHTML="";
-            for (let i=0;i<catObject.length;i++){
-                templateCard(i, catObject[i]);
-            }
-        localStorage.setItem('dataZelda',JSON.stringify(data));
-    }).catch(error => {
 
-        // This error object will have the error from the server
-        // As well as the two additions we made earlier of the status and statusText
-        
-        document.getElementById("templateCard").innerHTML=`<div class=errDiv><h1>${error}</h1><h2>Try it again later. Ganon is waiting for you!!</h2></div>`;
-    })
-        }
-
-};
-//función que muestra un elemento de la búsqueda
-function templateCard(id, objeto){
-    //creamos un div y le damos forma, introducimos los datos comunes a todas las categorías (nombre, imagen y icono like)
-    document.getElementById("templateCard").innerHTML+="<div class=card id="+id+"></div>";
-    document.getElementById("templateCard").style.margin="10px";
-    document.getElementById("templateCard").style.marginBottom="10px";
-    document.getElementById("templateCard").style.fontFamily="fantasy";
-    document.getElementById("templateCard").style.color="#624e29";
-    //en el nombre del item añadimos un icono para guardar ese elemento en favoritos, el icono llama una función "addFav"
-    document.getElementById(id).innerHTML+="<div class=className><h1>"+objeto.name+"</h1><img onclick='addFav("+id+")' src='./img/h3.png' alt='heartLike'></div>";
-    document.getElementById(id).innerHTML+=`<img src=${objeto.image} alt="zelda objet">`;
-    //como alguna elementos la localización en la API es null, hacemos este condicional
-    if(objeto.common_locations!=null){
-        document.getElementById(id).innerHTML+=("<p><b>Locations: </b> "+objeto.common_locations+"</p>").replace(",",", ");}
-    //cada categoría tiene información específica, así que dependiendo de la categoría del objeto añadimos unas cosas u otras al div
-    let catSelect= document.getElementById("selectCategory").value;
-    switch(catSelect){
-        case `creatures-food`:
-            document.getElementById(id).innerHTML+="<p><b>Cooking effect: </b>"+objeto.cooking_effect+"</p>";
-            document.getElementById(id).style.background="#fbeddfb2";
-            document.getElementById(id).style.border="solid #c58f99";            
-            break;
-        case `creatures-non-food`:
-            document.getElementById(id).innerHTML+=("<p><b>Drop: </b>"+objeto.drops+"</p>").replace(",",", ");
-            document.getElementById(id).style.background="#fbeddfb2";
-            document.getElementById(id).style.border="solid #2b4173";
-            break;
-        case `equipment`:
-            document.getElementById(id).innerHTML+="<p><b>Attack: </b>"+objeto.attack+"</p>";
-            document.getElementById(id).innerHTML+="<p><b>Defense: </b>"+objeto.defense+"</p>";
-            document.getElementById(id).style.background="#fbeddfb2";
-            document.getElementById(id).style.border="solid #471f18";
-            break;
-        case `materials`:
-            document.getElementById(id).innerHTML+="<p><b>Cooking effect: </b>"+objeto.cooking_effect+"</p>";
-            document.getElementById(id).innerHTML+="<p><b>Hearts recovered: </b>"+objeto.hearts_recovered+"</p>";
-            document.getElementById(id).style.background="#fbeddfb2";
-            document.getElementById(id).style.border="solid #4c94b4";
-            break;
-        case `monsters`:
-            document.getElementById(id).innerHTML+=("<p><b>Drop: </b>"+objeto.drops+"</p>").replace(",",", ");
-            document.getElementById(id).style.background="#fbeddfb2";
-            document.getElementById(id).style.border="solid #5a4c84";
-            break;
-        case `treasure`:
-            document.getElementById(id).innerHTML+=("<p><b>Drop: </b>"+objeto.drops+"</p>").replace(",",", ");
-            document.getElementById(id).style.background="#fbeddfb2";
-            document.getElementById(id).style.border="solid #ba9b63";
-            break;
-        default: 
-    }
-    //finalemente añadimos la descripción general que comparten todos los elementos
-    document.getElementById(id).innerHTML+="<p>"+objeto.description+"</p>";
-
-}
 
 //funcition para añadir favoritos 
 function addFav(id){
@@ -275,7 +292,7 @@ function resetFav(){
 //código que se ejecutará siempre al cargar la página. Comprueba si favs contiene elementos y si los tiene, los muestra
 if (localStorage.getItem('favs')!=null){document.getElementById('templateFavs').innerHTML+=localStorage.getItem('favs');
 }
-else{localStorage.favs="";
+else{localStorage.favs=""; //si no existe fav, lo inicializamos a un string vacío para después poder añadir favs
 }
 
 
